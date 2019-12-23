3ed8d9179e1a69b54e7cb1c973de0cc1dc8bf2f1
ShinAt <sinntarou836271@gmail.com>
2019-12-23 21:22:09 +0900

    itemモデルに対するテストを追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association Test
+  # item recordは単一のtodo recordに属しているかテスト
+  it { should belong_to(:todo) } 
+
+  # Validation test
+  # 保存する前にnameが存在するかテスト
+  it { should validate_presence_of(:name) }
 end
