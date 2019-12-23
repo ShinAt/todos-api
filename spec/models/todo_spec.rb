db127806fec2e1175f1acc4badafdb8e03001f01
ShinAt <sinntarou836271@gmail.com>
2019-12-23 20:41:34 +0900

    Todoモデルに対するテストを追加

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  # TodoモデルがItemモデルと1:mの関係があるかテスト
+  it { should have_many(:items).dependent(:destroy) } 
+
+  # Validation test
+  # 保存する前にtitleとcreated_byが存在するかテスト
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:created_by) }
 end
